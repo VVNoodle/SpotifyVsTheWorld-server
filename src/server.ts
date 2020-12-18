4c0271c26b33300f48b5a71d42be376afa9b51f2
Egan Bisma <brickkace@gmail.com>
2020-12-17 18:36:26 -0800

    first commit

---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import * as express from "express";
+import { ServeGrip } from "@fanoutio/serve-grip";
+import { WebSocketMessageFormat } from "@fanoutio/grip";
+
+const PORT = 3000;
+const CHANNEL_NAME = "test";
+
+const app = express();
+
+const serveGrip = new ServeGrip({
+  grip: {
+    control_uri: process.env.GRIP_URL,
+    control_iss: process.env.REALM_ID,
+    key: process.env.REALM_KEY,
+  },
+});
+
+app.use(serveGrip);
+
+// Websocket-over-HTTP is translated to HTTP POST
+app.post("/api/websocket", async (req, res) => {
+  const { wsContext } = req.grip;
+  if (wsContext == null) {
+    res.statusCode = 400;
+    res.end("[not a websocket request]\n");
+    return;
+  }
+
+  // If this is a new connection, accept it and subscribe it to a channel
+  if (wsContext.isOpening()) {
+    wsContext.accept();
+    wsContext.subscribe(CHANNEL_NAME);
+  }
+
+  while (wsContext.canRecv()) {
+    const message = wsContext.recv();
+
+    if (message == null) {
+      // If return value is undefined then connection is closed
+      wsContext.close();
+      break;
+    }
+
+    // Echo the message
+    wsContext.send(message);
+  }
+
+  res.end();
+});
+
+app.post("/api/broadcast", express.text({ type: "*/*" }), async (req, res) => {
+  const publisher = serveGrip.getPublisher();
+  await publisher.publishFormats(
+    CHANNEL_NAME,
+    new WebSocketMessageFormat(req.body)
+  );
+
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Ok\n");
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
