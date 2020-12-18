47b7796cd1c20748001275806f8fef51f6b5af78
Egan Bisma <brickkace@gmail.com>
2020-12-18 09:51:09 -0800

    moving server code

--- types/express/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { IRequestGrip } from "@fanoutio/serve-grip";
-
-declare module "express-serve-static-core" {
-  // first, declare that we are adding a method to `Response` (the interface)
-  export interface Request {
-    grip: IRequestGrip;
-  }
-}
