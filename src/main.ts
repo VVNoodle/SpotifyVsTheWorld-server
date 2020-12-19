4db7b8c16327a538011a227536e7dc2d9fd4ad67
Egan Bisma <brickkace@gmail.com>
2020-12-18 21:43:49 -0800

    adding error handling

--- src/main.ts
+++ src/main.ts
@@ -8,72 +8,87 @@ import {
   Publisher,
 } from '@fanoutio/grip';
 
-http
-  .createServer(async (req, res) => {
-    // Validate the Grip-Sig header:
-    if (
-      !validateSig(req.headers['grip-sig'] as string, process.env.REALM_KEY)
-    ) {
-      res.writeHead(401);
-      res.end('invalid grip-sig token');
-      return;
-    }
+try {
+  http
+    .createServer(async (req, res) => {
+      try {
+        // Validate the Grip-Sig header:
+        if (
+          !validateSig(req.headers['grip-sig'] as string, process.env.REALM_KEY)
+        ) {
+          res.writeHead(401);
+          res.end('invalid grip-sig token');
+          return;
+        }
 
-    // Make sure we have a connection ID
-    let cid = req.headers['connection-id'];
-    if (Array.isArray(cid)) {
-      cid = cid[0];
-    }
-    if (req.headers['connection-id'] == null) {
-      res.writeHead(401);
-      res.end('connection-id required');
-      return;
-    }
+        // Make sure we have a connection ID
+        let cid = req.headers['connection-id'];
+        if (Array.isArray(cid)) {
+          cid = cid[0];
+        }
+        if (req.headers['connection-id'] == null) {
+          res.writeHead(401);
+          res.end('connection-id required');
+          return;
+        }
 
-    const inEventsEncoded = await new Promise((resolve) => {
-      let body = '';
-      req.on('data', function (chunk) {
-        body += chunk;
-      });
-      req.on('end', function () {
-        resolve(body);
-      });
-    });
-
-    const inEvents = decodeWebSocketEvents(inEventsEncoded as string | Buffer);
-    const wsContext = new WebSocketContext(cid, {}, inEvents);
-
-    if (wsContext.isOpening()) {
-      // Open the WebSocket and subscribe it to a channel:
-      wsContext.accept();
-      wsContext.subscribe('test');
-
-      // The above commands made to the wsContext are buffered
-      // in the wsContext as "outgoing events".
-      // Obtain them and write them to the response.
-      const outEvents = wsContext.getOutgoingEvents();
-      const outEventsEncoded = encodeWebSocketEvents(outEvents);
-      res.write(outEventsEncoded);
-
-      // As an example way to check our subscription, wait and then
-      // publish a message to the subscribed channel:
-      setTimeout(() => {
-        const publisher = new Publisher({
-          control_uri: process.env.GRIP_URL,
-          control_iss: process.env.REALM_ID,
-          key: Buffer.from(process.env.REALM_KEY, 'base64'),
+        const inEventsEncoded = await new Promise((resolve) => {
+          let body = '';
+          req.on('data', function (chunk) {
+            body += chunk;
+          });
+          req.on('end', function () {
+            resolve(body);
+          });
         });
-        publisher.publishFormats(
-          'test',
-          new WebSocketMessageFormat('Test WebSocket Publish!!'),
+
+        const inEvents = decodeWebSocketEvents(
+          inEventsEncoded as string | Buffer,
         );
-      }, 5000);
-    }
+        const wsContext = new WebSocketContext(cid, {}, inEvents);
 
-    // Set the headers required by the GRIP proxy:
-    res.writeHead(200, wsContext.toHeaders());
-    res.end();
-  })
-  .listen(parseInt(process.env.PORT), '0.0.0.0');
+        if (wsContext.isOpening()) {
+          // Open the WebSocket and subscribe it to a channel:
+          wsContext.accept();
+          wsContext.subscribe('test');
 
-console.log('Server running...');
+          // The above commands made to the wsContext are buffered
+          // in the wsContext as "outgoing events".
+          // Obtain them and write them to the response.
+          const outEvents = wsContext.getOutgoingEvents();
+          const outEventsEncoded = encodeWebSocketEvents(outEvents);
+          res.write(outEventsEncoded);
+
+          // As an example way to check our subscription, wait and then
+          // publish a message to the subscribed channel:
+          setTimeout(() => {
+            const publisher = new Publisher({
+              control_uri: process.env.GRIP_URL,
+              control_iss: process.env.REALM_ID,
+              key: Buffer.from(process.env.REALM_KEY, 'base64'),
+            });
+            publisher.publishFormats(
+              'test',
+              new WebSocketMessageFormat('Test WebSocket Publish!!'),
+            );
+          }, 5000);
+        }
+
+        // Set the headers required by the GRIP proxy:
+        res.writeHead(200, wsContext.toHeaders());
+      } catch (error) {
+        console.log('oops, theres error:', error);
+      }
+      res.end();
+    })
+    .listen(parseInt(process.env.PORT), '0.0.0.0');
+  console.log('Server running...');
+} catch (error) {
+  console.log('Server NOT running...');
+  console.log('ERROR', error);
+  try {
+    console.log('ERROR', error.message);
+  } catch (err) {
+    console.log('foo no', err);
+  }
+}
