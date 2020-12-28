6a04d181eb81918b09f42bb2f51c59426a1d1ce1
Egan Bisma <brickkace@gmail.com>
2020-12-28 00:05:12 -0800

    add more logic to onDisconnect + add docs

--- src/utils/onDisconnect.ts
+++ src/utils/onDisconnect.ts
@@ -5,6 +5,15 @@ import {
 } from '@fanoutio/grip';
 import { Redis } from 'ioredis';
 
+/**
+ * if client is listening to a track
+ * decrement the artist listener count, unsubscribe client, and
+ * delete client entry from client hash
+ * Finally, publish the updated listener count
+ * @param redis the redis connection instance
+ * @param pub the publisher helper util
+ * @param wsContext wsContext helper util
+ */
 export const onDisconnect = async (
   redis: Redis,
   pub: Publisher,
@@ -12,7 +21,17 @@ export const onDisconnect = async (
 ): Promise<void> => {
   console.log(`client with id ${wsContext.id} has disconnected`);
   const artistLastListened = await redis.hget('client', wsContext.id);
+
+  if (!artistLastListened) {
+    console.log('client is not listening to anything');
+    return;
+  }
+
   const listenerCount = await redis.hincrby('artist', artistLastListened, -1);
+
+  wsContext.unsubscribe(artistLastListened);
+  await redis.hdel('client', wsContext.id);
+
   pub.publishFormats(
     artistLastListened,
     new WebSocketMessageFormat(listenerCount.toString()),
--- src/utils/onText.ts
+++ src/utils/onText.ts
@@ -1,6 +1,4 @@
-// import * as uWS from 'uWebSockets.js';
 import {
-  // encodeWebSocketEvents,
   Publisher,
   WebSocketContext,
   WebSocketMessageFormat,
