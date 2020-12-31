bd619f1cfff0c4322bc2916675b54914120e31fe
Egan Bisma <brickkace@gmail.com>
2020-12-31 13:30:07 -0800

    added queue for ratelimitting

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,41 @@
 import * as uWS from 'uWebSockets.js';
-import * as Redis from 'ioredis';
+import * as IORedis from 'ioredis';
 import { Request } from 'express';
 import { IRequestGrip, ServeGrip } from '@fanoutio/serve-grip';
 import {
   decodeWebSocketEvents,
   encodeWebSocketEvents,
-  parseGripUri,
   WebSocketContext,
 } from '@fanoutio/grip';
+import { Queue, QueueScheduler, Worker } from 'bullmq';
 import { readData } from './utils/readData';
 import { onDisconnect } from './utils/onDisconnect';
 import { onText } from './utils/onText';
+import * as path from 'path';
 
-const redis = new Redis({
+const redis = new IORedis({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT),
 });
-const config = parseGripUri(process.env.GRIP_URL);
 const serveGrip = new ServeGrip({
-  grip: config,
+  grip: {
+    control_uri: 'http://127.0.0.1:5561',
+    key: 'changeme',
+  },
 });
+
+const jobName = 'publisherJob';
+const publishQueue = new Queue(jobName, {
+  connection: redis,
+});
+const processorFile = path.join(__dirname, 'workers', 'publishMessages.ts');
+new Worker(jobName, processorFile, {
+  limiter: {
+    max: 10,
+    duration: 1000,
+  },
+});
+new QueueScheduler(jobName);
 
 interface CustomRequest extends uWS.HttpRequest, Request {
   grip: IRequestGrip;
@@ -67,13 +83,6 @@ uWS
     }
     req.body = data;
 
-    // // Validate the Grip-Sig header:
-    // if (!validateSig(req.headers['grip-sig'], process.env.REALM_KEY)) {
-    //   res.writeHead(401);
-    //   res.end('invalid grip-sig token');
-    //   return;
-    // }
-
     // Make sure we have a connection ID
     let cid = req.headers['connection-id'];
     if (Array.isArray(cid)) {
@@ -100,6 +109,7 @@ uWS
 
     const pub = serveGrip.getPublisher();
 
+    const body: (string | void)[] = [];
     for (let i = 0; i < wsContext.inEvents.length; i++) {
       const { type, content } = wsContext.inEvents[i];
 
@@ -108,31 +118,44 @@ uWS
         console.log(`client with id ${wsContext.id} has connected`);
       } else if (type === 'DISCONNECT') {
         await onDisconnect(redis, pub, wsContext);
-      } else if (type === 'TEXT') {
-        if (content) {
-          await onText(redis, pub, wsContext, content);
-        }
+      } else if (type === 'TEXT' && content) {
+        body.push(await onText(res, redis, pub, wsContext, content));
       } else if (type === 'CLOSE') {
         console.log('client disconnects on purpose');
         await onDisconnect(redis, pub, wsContext);
       }
     }
 
-    function writeHeaders(res: uWS.HttpResponse) {
-      const headers = wsContext.toHeaders();
-      console.log(headers);
-      Object.keys(headers).forEach((key) => {
-        res.writeHeader(key, headers[key]);
+    res.cork(async () => {
+      res = writeHeaders(res, wsContext);
+      res
+        .writeStatus('200 OK')
+        .end(encodeWebSocketEvents(wsContext.getOutgoingEvents()));
+
+      if (!body) {
+        return;
+      }
+      const addAsyncJobs = body.map(async (stuff) => {
+        if (typeof stuff === 'string' && stuff.length) {
+          await publishQueue.add(stuff, stuff, {
+            delay: 5000,
+          });
+        }
       });
-      return res;
-    }
-    res = writeHeaders(res);
-    res
-      .writeStatus('200 OK')
-      .end(encodeWebSocketEvents(wsContext.getOutgoingEvents()));
+      await Promise.all(addAsyncJobs);
+    });
   })
   .listen(parseInt(process.env.PORT), (listenSocket) => {
     if (listenSocket) {
       console.log(`Listening to port ${process.env.PORT}`);
     }
   });
+
+function writeHeaders(res: uWS.HttpResponse, wsContext: WebSocketContext) {
+  const headers = wsContext.toHeaders();
+  console.log(headers);
+  Object.keys(headers).forEach((key) => {
+    res.writeHeader(key, headers[key]);
+  });
+  return res;
+}
--- src/utils/onText.ts
+++ src/utils/onText.ts
@@ -1,9 +1,8 @@
-import {
-  Publisher,
-  WebSocketContext,
-  WebSocketMessageFormat,
-} from '@fanoutio/grip';
+import * as uWS from 'uWebSockets.js';
+import { Publisher, WebSocketContext } from '@fanoutio/grip';
 import { Redis } from 'ioredis';
+
+const RANDOM_NUMBER = -9;
 
 /**
  * decrement listener count of previous artist and publish the new count
@@ -11,12 +10,14 @@ import { Redis } from 'ioredis';
  * @param content message sent from client. Expected to be in the following format: 'previousArtistName,currentArtistName'
  */
 export const onText = async (
-  // res: uWS.HttpResponse,
+  res: uWS.HttpResponse,
   redis: Redis,
   pub: Publisher,
   wsContext: WebSocketContext,
   content: string | Buffer | number[],
-): Promise<void> => {
+): Promise<string | void> => {
+  console.log(res);
+  console.log(pub);
   const contentString = content.toString();
   console.log('content', contentString);
   if (!contentString.includes('||')) {
@@ -31,54 +32,74 @@ export const onText = async (
     await redis.hset('client', wsContext.id, currentArtist);
 
     // unsub from previousArtist first. THEN publish new value. Order matters
+    let previousListenerCount = RANDOM_NUMBER;
+    let currentListenerCount = RANDOM_NUMBER;
+    wsContext.subscribe(currentArtist);
     if (previousArtist && previousArtist.length) {
       console.log(
         `unsubscribing from ${previousArtist}. subscribing to ${currentArtist}`,
       );
       wsContext.unsubscribe(previousArtist);
-
-      const previousListenerCount = await redis.hincrby(
-        'artist',
-        previousArtist,
-        -1,
-      );
-
-      try {
-        pub.publishFormats(
-          previousArtist,
-          new WebSocketMessageFormat(previousListenerCount.toString()),
-        );
-        console.log('Publish successful!', previousListenerCount.toString());
-      } catch (ex) {
-        if (ex.message) {
-          console.log('Publish failed!');
-          console.log('Message: ' + ex.message);
-          console.log('Context: ');
-          console.dir(ex.context);
-        } else {
-          throw ex;
-        }
-      }
+      await redis
+        .multi()
+        .hincrby('artist', currentArtist, 1)
+        .hincrby('artist', previousArtist, -1)
+        .exec((err, results) => {
+          if (err) {
+            console.log('error', err);
+          }
+          previousListenerCount = results[1][1];
+        });
     } else {
       console.log(`first artist. nothing to unsubscribe`);
+      currentListenerCount = await redis.hincrby('artist', currentArtist, 1);
     }
-
     wsContext.subscribe(currentArtist);
-    const currentListenerCount = await redis.hincrby(
-      'artist',
-      currentArtist,
-      1,
+
+    // // The above commands made to the wsContext are buffered
+    // // in the wsContext as "outgoing events".
+    // // Obtain them and write them to the response.
+    // const outEvents = wsContext.getOutgoingEvents();
+    // const outEventsEncoded = encodeWebSocketEvents(outEvents);
+    // res.write(outEventsEncoded);
+
+    console.log(
+      `listener count for ${previousArtist}: ${previousListenerCount.toString()}`,
     );
-    console.log('listener count', currentListenerCount.toString());
+    console.log(
+      `listener count for ${currentArtist}: ${currentListenerCount.toString()}`,
+    );
     try {
-      setTimeout(() => {
-        pub.publishFormats(
-          currentArtist,
-          new WebSocketMessageFormat(currentListenerCount.toString()),
-        );
-      }, 3000);
-      console.log('Publish successful!');
+      const items = [
+        {
+          channel: currentArtist,
+          formats: {
+            'ws-message': {
+              content: currentListenerCount.toString(),
+            },
+          },
+        },
+      ];
+      if (previousArtist && previousArtist.length) {
+        items.push({
+          channel: previousArtist,
+          formats: {
+            'ws-message': {
+              content: previousListenerCount.toString(),
+            },
+          },
+        });
+      }
+      const body = JSON.stringify({
+        items,
+      });
+      return body;
+
+      // const json = await response.json();
+      // console.log('json response', json);
+      // console.log('Publish successful!');
     } catch (ex) {
+      console.log(ex);
       if (ex.message) {
         console.log('Publish failed!');
         console.log('Message: ' + ex.message);
