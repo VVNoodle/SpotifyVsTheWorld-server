ffc946038b1538edfd72cf62df6cda5e73349203
Egan Bisma <brickkace@gmail.com>
2021-01-16 11:55:59 -0800

    add back broadcast for unsub

--- src/utils/unsub.ts
+++ src/utils/unsub.ts
@@ -1,4 +1,6 @@
 import IORedis from 'ioredis';
+import fetch from 'node-fetch';
+
 async function unsub(
   redis: IORedis.Redis,
   artistName: string,
@@ -7,6 +9,15 @@ async function unsub(
     const listenerCount = await redis.hincrby(artistName, 'count', -1);
     const listenerCountResponse = `c=${listenerCount}`;
     console.log(`unsub value: ${listenerCountResponse}`);
+    const response = await fetch(
+      `https://${process.env.NCHAN_URL}/pubsub/${artistName}`,
+      {
+        method: 'POST',
+        body: listenerCountResponse,
+      },
+    );
+    await response.text();
+
     return listenerCountResponse;
   } catch (error) {
     console.log('error is', error);
